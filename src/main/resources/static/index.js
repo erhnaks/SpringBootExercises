fd1d06e31c3b55d1db1ee0f9cd424fb3724ce36f
erhnaks <erhnaks@hotmail.com>
2022-07-13 16:18:10 +0100

    front-end work has been added in the src main static folder

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -1,80 +1,84 @@
 "use strict";
 
-document.querySelector("body > form").addEventListener("submit", function (event) {
-    event.preventDefault(); // turns off the page refresh/redirect
-    
+document.querySelector("form").addEventListener("submit", function (event) {
+  event.preventDefault(); // turns off the page refresh/redirect
 
-    console.log("THIS:", this);
+  console.log("THIS:", this);
 
-    const data = {
-        brand: this.brand.value,
-        fuel: this.fuel.value,
-        engine: this.engine.value
-    }
+  const data = {
+    brand: this.brand.value,
+    fuel: this.fuel.value,
+    engine: this.engine.value,
+  };
 
-    console.log("DATA:", data);
+  console.log("DATA:", data);
 
-    axios.post("http://localhost:8080/createCar", data)
-            .then(res => {
-                console.log("RES:", res);
-                this.reset();
-                this.brand.focus();
-                renderCars();
-            })
-            .catch(err => console.error(err));
+  axios
+    .post("http://localhost:8080/createCar", data)
+    .then((res) => {
+      console.log("RES:", res);
+      this.reset();
+      this.brand.focus();
+      renderCars();
+    })
+    .catch((err) => console.error(err));
 });
 
 const output = document.querySelector("#output");
 
 function renderCars() {
-    axios.get("http://localhost:8080/carGetAll")
-        .then(res => {
-            console.log("cars: ", res.data);
-            output.innerHTML = "";
-            for (let car of res.data) {
-                const carCol = document.createElement("div");
-                carCol.className = "col";
+  axios
+    .get("http://localhost:8080/carGetAll")
+    .then((res) => {
+      console.log("cars: ", res.data);
+      output.innerHTML = "";
+      for (let car of res.data) {
+        const carCol = document.createElement("div");
+        carCol.className = "col";
 
-                const carCard = document.createElement("div");
-                carCard.className = "card";
-                carCol.appendChild(carCard);
+        const carCard = document.createElement("div");
+        carCard.className = "card";
+        carCol.appendChild(carCard);
 
-                const carDiv = document.createElement("div");
-                carDiv.className = "card-body";
-                carCard.appendChild(carDiv);
+        const carDiv = document.createElement("div");
+        carDiv.className = "card-body";
+        carCard.appendChild(carDiv);
 
-                const carBrand = document.createElement("h2");
-                carBrand.innerText = car.brand;
-                carDiv.appendChild(carBrand);
+        const carBrand = document.createElement("h2");
+        carBrand.innerText = car.brand;
+        carDiv.appendChild(carBrand);
 
-                const carFuel = document.createElement("p");
-                carFuel.innerText ="Fuel: " + car.fuel;
-                carDiv.appendChild(carFuel);
+        const carFuel = document.createElement("p");
+        carFuel.innerText = "Fuel: " + car.fuel;
+        carDiv.appendChild(carFuel);
 
-                const carEngine = document.createElement("p");
-                carEngine.innerText = "Engine: " + car.engine;
-                carDiv.appendChild(carEngine);
+        const carEngine = document.createElement("p");
+        carEngine.innerText = "Engine: " + car.engine;
+        carDiv.appendChild(carEngine);
 
-                const carDelete = document.createElement('button');
-                carDelete.innerText = "DELETE";
-                carDelete.addEventListener("click", () => {
-                    console.log("CAR: ", car);
-                    deleteCar(car.id);
-                });
-                carDiv.appendChild(carDelete);
+        const carDelete = document.createElement("button");
+        carDelete.innerText = "DELETE";
+        carDelete.classList.add("btn", "btn-danger");
+        carDelete.addEventListener("click", () => {
+        console.log("Car: ", car);
+        deleteCar(car.id)
+        });
+        carDiv.appendChild(carDelete);
 
-                output.appendChild(carCol);
-            }
-        })
-        .catch(err => console.error(err));
+        output.appendChild(carCol);
+      }
+    })
+    .catch((err) => console.error(err));
 }
 
-const deleteDino = (id) => {
-    axios.delete("http://localhost:8080/removeCar/" + id)
-            .then(res => {
-                console.log("Delete successful");
-                renderCars();
-            }).catch(err => console.error(err));
-}
+const deleteCar = (id) => {
+  axios
+    .delete("http://localhost:8080/removeCar/" + id)
+    .then((res) => {
+      console.log("Delete successful");
+      renderCars();
+    })
+    .catch((err) => console.error(err));
+};
 
 renderCars();
